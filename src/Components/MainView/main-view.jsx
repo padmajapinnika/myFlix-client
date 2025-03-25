2380263c5a69d915aa5467b3bbf8053fc549e6d6
padmajapinnika <padmajapinnika@gmail.com>
2025-03-24 23:29:29 -0400

    adding api to react app
    Display complete movie details including genre, director, actors, release year, and description.

--- src/Components/MainView/main-view.jsx
+++ src/Components/MainView/main-view.jsx
@@ -1,55 +1,61 @@
-import { useState } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
-export const MainView = () => {
-  const [movies, setMovies] = useState([
-    {
-        id: 1,
-        title: "Inception",
-        description: "A skilled thief is given a chance at redemption if he can successfully perform an inception.",
-        posterUrl: "https://image.tmdb.org/t/p/w500/edv5CZvGlj0u5fN1YQO1a6lRU6F.jpg",
-        genre: "Sci-Fi, Action",
-        director: "Christopher Nolan",
-        year: 2010
-      },
-      {
-        id: 2,
-        title: "The Matrix",
-        description: "A hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.",
-        posterUrl: "https://image.tmdb.org/t/p/w500/8cFmhV7DRe2Nk4mr7NkK0UzcGKv.jpg",
-        genre: "Action, Sci-Fi",
-        director: "The Wachowskis",
-        year: 1999
-      },
-      {
-        id: 3,
-        title: "The Dark Knight",
-        description: "Batman faces the Joker, a criminal mastermind who seeks to plunge Gotham City into anarchy.",
-        posterUrl: "https://image.tmdb.org/t/p/w500/qJ2tW6WMUDux911r6m7haRef0WH.jpg",
-        genre: "Action, Crime, Drama",
-        director: "Christopher Nolan",
-        year: 2008
-      }
-    ]);
+import { useState, useEffect } from "react";
 
-  
+export const MainView = () => {
+    const [movies, setMovies] = useState([]);
     const [selectedMovie, setSelectedMovie] = useState(null);
-    const handleBackClick = () => {
-        setSelectedMovie(null); // Reset selected movie to null to go back to the movie list
-      };
+    const url = "https://movie-api-padma-7528be21ca05.herokuapp.com/movies"
+
+    useEffect(() => {
+        fetch(url)
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                setMovies(data);
+            });
+    },[]);
+
+    if (selectedMovie) {
+        let similarMovies = movies.filter((movie) => {
+            return movie.genre.name === selectedMovie.genre.name;
+        })
+        console.log("Similar Movies:",similarMovies);
+        return (
+            <div>
+                <MovieView
+                    movie={selectedMovie}
+                    onBackClick={() => setSelectedMovie(null)} />
+                <hr />
+                <h2>Similar Movies</h2>
+                {similarMovies.map((movie) => (
+                    <MovieCard
+                        key={movie._id}
+                        movie={movie}
+                        onMovieClick={(newSelection) => {
+                            setSelectedMovie(newSelection);
+                        }}
+                    />
+                ))}
+            </div>
+        );
+    }
+
+    if (movies.length === 0) {
+        return <div>The list is empty!</div>;
+    }
+
     return (
-      <div>
-        {selectedMovie ? (
-          <MovieView movie={selectedMovie} onBackClick={handleBackClick}/>
-        ) : (
-          <div>
+        <div>
             {movies.map((movie) => (
-              <div key={movie.id} onClick={() => setSelectedMovie(movie)}>
-                <h3>{movie.title}</h3>
-              </div>
+                <MovieCard
+                    key={movie._id}
+                    movie={movie}
+                    onMovieClick={(newSelection) => {
+                        setSelectedMovie(newSelection);
+                    }}
+                />
             ))}
-          </div>
-        )}
-      </div>
+        </div>
     );
-  };
+};
--- src/Components/movie-view/movie-view.jsx
+++ src/Components/movie-view/movie-view.jsx
@@ -1,13 +1,28 @@
+import PropTypes from 'prop-types';
 export const MovieView = ({ movie, onBackClick }) => {
     return (
       <div className="movie-view">
-        <h2>{movie.title}</h2>
+        <h2>{movie?.title}</h2>
         <img src={movie.posterUrl} alt={movie.title} className="movie-poster" />
-        <p><strong>Description:</strong> {movie.description}</p>
-        <p><strong>Genre:</strong> {movie.genre}</p>
+        <p><strong>Description:</strong> {movie.description}</p>    
         <p><strong>Director:</strong> {movie.director}</p>
-        <p><strong>Release Year:</strong> {movie.year}</p>
-        <button onClick={onBackClick}>Back to Movie List</button>
+        <p><strong>Genre:</strong> {movie.genre || 'Not Available'}</p>
+        <p><strong>Release Year:</strong> {movie.releaseYear|| 'Not Available'}</p>
+        <button onClick={onBackClick}>Back to Movie List</button>       <button onClick={onBackClick}>Back to Movie List</button>
       </div>
     );
+  };
+  MovieView.propTypes = {
+    movie: PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+      genre: PropTypes.shape({
+        name: PropTypes.string,
+      }),
+      director: PropTypes.shape({
+        name: PropTypes.string,
+      }),
+      releaseYear: PropTypes.number,
+    }).isRequired,
+    onBackClick: PropTypes.func.isRequired,
   };
